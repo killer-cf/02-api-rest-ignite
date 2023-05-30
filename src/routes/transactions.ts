03b8219bcf3d64260b2575281a4a2e55665fa7ba
killer-cf <costa.kilder@live.com>
2023-05-30 19:19:40 -0300

    user can get transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,41 @@ import { prisma } from '../lib/prisma'
 import { z } from 'zod'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await prisma.transaction.findMany()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (req) => {
+    const setTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = setTransactionParamsSchema.parse(req.params)
+
+    const transaction = await prisma.transaction.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return { transaction }
+  })
+
+  app.get('/summary', async () => {
+    const summary = (await prisma.transaction.findMany()).reduce(
+      (acc, transaction) => {
+        acc = acc + Number(transaction.amount)
+
+        return acc
+      },
+      0.0,
+    )
+
+    return { summary }
+  })
+
   app.post('/', async (req, res) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
